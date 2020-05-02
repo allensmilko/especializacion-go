d1af4f2598a6b003c359124d1f334341dde4208d
Jeison camilo Castñeda puerta <jeison.castaneda@rappi.com>
2020-05-01 19:29:01 -0500

    fix review

--- trunc/trunc.go
+++ trunc/trunc.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
-var forTrunc float64 = 10.2
+var forTrunc float64
 var intNum int
 
 func truncateInt(y float64) int {
@@ -11,15 +16,30 @@ func truncateInt(y float64) int {
 }
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Println("Step 1")
+	fmt.Println("Enter first float value : ")
+	scanner.Scan()
+	input := scanner.Text()
+	forTrunc, err := strconv.ParseFloat(input, 64)
 
+	if err != nil {
+		fmt.Println(err)
+	}
 	intNum = truncateInt(forTrunc)
 	fmt.Println(intNum)
 
 	fmt.Println("/////////")
 
 	fmt.Println("Step 2")
+	fmt.Println("Enter second float value : ")
+	scanner.Scan()
+	input = scanner.Text()
+	forTrunc, err = strconv.ParseFloat(input, 64)
 
+	if err != nil {
+		fmt.Println(err)
+	}
 	forTrunc = 6.2
 	intNum = truncateInt(forTrunc)
 	fmt.Println(intNum)
